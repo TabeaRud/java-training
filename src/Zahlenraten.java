783f54ae5783837a675e3093381f3ebc024d940a
Tabea Rudersdorfer <tabeaar@web.de>
2026-09-24 13:29:10 +0200

    Add Zahlenraten

---
+++ src/Zahlenraten.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zahlenraten {
+    static void main() {
+        Random zufall = new Random();
+        Scanner sc = new Scanner(System.in);
+        int geheimzahl = zufall.nextInt(100) + 1;
+
+        int versuche = 0;
+        boolean erraten = false;
+
+        while (!erraten) {
+            System.out.println("Bitte nenne eine Zahl zwischen 1 und 100: ");
+            String guessInput = sc.nextLine().trim();
+            int guess = Integer.parseInt(guessInput);
+            versuche++;
+            if (guess < geheimzahl) {
+                System.out.println("Zu klein.");
+            } else if (guess > geheimzahl) {
+                System.out.println("Zu groß.");
+            } else if (guess == geheimzahl) {
+                System.out.println("Richtig geraten!");
+                break;
+            }
+        }
+        System.out.println("Erraten in " + versuche + " Versuchen.");
+    }
+}
