41987362a553150501f3ff47072c07b0a009db02
Tabea Rudersdorfer <tabeaar@web.de>
2026-09-25 10:13:12 +0200

    Split Zahlenraten into methods

--- src/Zahlenraten.java
+++ src/Zahlenraten.java
@@ -5,25 +5,43 @@ public class Zahlenraten {
     static void main() {
         Random zufall = new Random();
         Scanner sc = new Scanner(System.in);
-        int geheimzahl = zufall.nextInt(100) + 1;
-
+        int geheim = zufall.nextInt(100) + 1;
         int versuche = 0;
         boolean erraten = false;
 
+
         while (!erraten) {
-            System.out.println("Bitte nenne eine Zahl zwischen 1 und 100: ");
-            String guessInput = sc.nextLine().trim();
-            int guess = Integer.parseInt(guessInput);
-            versuche++;
-            if (guess < geheimzahl) {
-                System.out.println("Zu klein.");
-            } else if (guess > geheimzahl) {
-                System.out.println("Zu groß.");
-            } else if (guess == geheimzahl) {
-                System.out.println("Richtig geraten!");
+            int geraten = liesZahl(sc, "Bitte nenne eine Zahl zwischen 1 und 100: ");
+            String bewertung = bewerte(geraten, geheim);
+            System.out.println(bewertung);
+
+            if (bewertung.equalsIgnoreCase("Richtig geraten!")) {
                 erraten = true;
             }
+            versuche++;
         }
+        druckeErgebnis(versuche);
+    }
+
+    static int liesZahl(Scanner in, String frage) {
+        System.out.println(frage);
+        String input = in.nextLine().trim();
+        int geraten = Integer.parseInt(input);
+        return geraten;
+    }
+
+    static String bewerte(int geraten, int geheim) {
+        if (geraten < geheim) {
+            return "Zu klein.";
+        } else if (geraten > geheim) {
+            return "Zu groß.";
+        } else {
+            return "Richtig geraten!";
+        }
+    }
+
+    static void druckeErgebnis(int versuche) {
         System.out.println("Erraten in " + versuche + " Versuchen.");
     }
 }
+
