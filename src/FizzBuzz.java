c36b0bd80ae82629f5f144de70c05b4cd8a53d10
TabeaRud <tabeaar@web.de>
2026-09-25 09:16:28 +0200

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,12 +1,12 @@
 public class FizzBuzz {
     static void main() {
         for (int i = 1; i < 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+            } else if  (i % 3 == 0) {
+                System.out.println("Fizz");
             } else {
                 System.out.println(i);
             }
